d6a072da321b1cd1df5c45b2dc34022f70115ae7
ErickTorres <erickcanvas@gmail.com>
2024-01-13 06:54:08 -0300

    Added CanvasDEVEditor core package
    My package to reference editor keys

---
+++ Assets/_Project/Scripts/Editor/Core/CanvasDevEditorKeys.cs
@@ -0,0 +1,13 @@
+namespace CanvasDEV.Editor.Core
+{
+    public static class CanvasDevEditorKeys
+    {
+        public const string ToolsPath = "Tools/CanvasDEV/";
+
+        public static class RenameAssets
+        {
+            public static readonly string ReferenceNameKey = "RenameAssetsReferenceNameKey";
+            public static readonly string NewNameKey = "RenameAssetsNewNameKey";
+        }
+    }
+}
