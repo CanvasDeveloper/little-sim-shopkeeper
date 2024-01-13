db6238612812ff85614fa7ec92a8dc379ab535dd
ErickTorres <erickcanvas@gmail.com>
2024-01-13 07:13:49 -0300

    Added button base

---
+++ Assets/_Project/Scripts/Runtime/Systems/UI/ButtonBase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CanvasDEV.Runtime.Systems.UI.Core
+{
+    public abstract class ButtonBase : MonoBehaviour
+    {
+        protected Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(ButtonBehaviour);
+        }
+
+        protected abstract void ButtonBehaviour();
+    }
+}
