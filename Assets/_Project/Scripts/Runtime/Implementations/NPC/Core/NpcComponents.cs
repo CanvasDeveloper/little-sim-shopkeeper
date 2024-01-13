b28460cc7a64583424bd31e4c033a446cea32e01
ErickTorres <erickcanvas@gmail.com>
2024-01-13 18:38:17 -0300

    Added temporary npc components

---
+++ Assets/_Project/Scripts/Runtime/Implementations/NPC/Core/NpcComponents.cs
@@ -0,0 +1,16 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class NpcComponents : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody2D { get; private set; }
+
+    [field: Required]
+    [field: SerializeField] public AnimationValues AnimationValues { get; private set; }
+
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/Implementations/NPC/StateMachine/States/NpcMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class NpcMovement : NpcStateBase
+{
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float runningSpeed = 1f;
+
+    private Vector2 _movementDirection;
+    private bool _isRunning;
+
+    public override void OnUpdate()
+    {
+        //_movementInput = machine.Components.Input.Movement;
+        //_isRunning = machine.Components.Input.RunToggle;
+
+        var cachedAnimationComponent = machine.Components.AnimationValues;
+
+        //bool isMoving = _movementInput.magnitude != 0f;
+
+        //if (isMoving)
+        //{
+        //    cachedAnimationComponent.XDirection = _movementInput.x;
+        //    cachedAnimationComponent.YDirection = _movementInput.y;
+        //}
+
+        //cachedAnimationComponent.OnMovement = isMoving;
+        //cachedAnimationComponent.IsRunning = _isRunning;
+    }
+
+    public override void OnFixedUpdate()
+    {
+        //var cachedRigidbody = machine.Components.Rigidbody2D;
+        //var desiredSpeed = _isRunning ? runningSpeed : moveSpeed;
+
+        //Vector2 newPosition = cachedRigidbody.position + desiredSpeed * Time.fixedDeltaTime * _movementDirection;
+        //cachedRigidbody.MovePosition(newPosition);
+    }
+}
